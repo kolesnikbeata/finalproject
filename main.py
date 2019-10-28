5bcf08fefbbc25dffaec08644372849485e654bd
Beata Kolesnik <beata.marinovskaja@gmail.com>
2019-10-28 21:21:25 +0200

    delete blank lines, fix hardcoded values problem

--- main.py
+++ main.py
@@ -4,19 +4,13 @@ import sys
 
 print("\nEncryption machine is starting...")
 print("\nSelect the option for text for encryption input:")
-
-
-
 whattodo = str(input("\nDo you want to encrypt text from file or to write your own text into cmnd line? (File/CMND/Q - quit) "))
-# print(whattodo)
 
 while whattodo.upper() not in ("Q","FILE","CMND"):
     print("Input was not recognised!!! ")
     whattodo = input("\nDo you want to encrypt text from file or to write your own text into cmnd line? (File/CMND/Q - quit) ")
 
-
 if whattodo.upper() == "FILE":
-    # import text from file
     filename = "texttoencrypt.txt"
     openedfile = open(filename, "r")
     t2e = openedfile.read()
@@ -39,71 +33,40 @@ al = " §±!@#$%^&*()_+-={[]}\?/.>,<~`;:\'|AqQBCDEFGHIJKLMNOPRSTUVWabcdefghijklm
 allist = []
 for i in al:
     allist.append(i)
-# print(allist)
 alphasize = len(allist)
-# print(alphasize)
-
 print("\nPlease wait...")
 print("\nEncryption machine is working on ciphering your text...")
 
-# print("tekstas kodavimui: ",t2e)
 key = []
 for i in t2e:
     key.append(random.randrange(0,len(allist)+1))
-# print("key", key)
-
-# def alphacalculator(list,number):
 
 t2eindexed = [] 
 for c in t2e:
     t2eindexed.append(allist.index(c))
-# print("t2eindexed", t2eindexed)
-
 
 encryptedindexes = []
 withkey = zip(t2eindexed,key)
 for a,b in  withkey:
     encryptedindexes.append(a-b)
-# print("indexes for cypher", encryptedindexes)
 
 encriptedtext = []
 for i in encryptedindexes:
     encriptedtext.append(allist[i])
-
-for b,element in enumerate(encriptedtext):
-    if element == ' ':
-        # print("found")
-        encriptedtext[b] = "kodiniszodis"
-
-# print("encryptedtext", encriptedtext)
-encriptedtext2 = ' '.join(encriptedtext)
-
-encriptedtext2 = encriptedtext2.replace(" ","").replace("kodiniszodis"," ")
-# print("encriptedtext2",encriptedtext2)
+encriptedtext2 = ''.join(encriptedtext)
 Encryptedmessage.write(encriptedtext2)
 print("""
 """)
 print("**************************************************************************")
 print("Your text was encrypted. Find encrypted text in Encrypted_message.txt file")
 print("**************************************************************************")
-
 print("""
 """)
-
-
-
-
-
-
 for i in t2e:
     key.append(random.randrange(0,len(allist)+1))
-# print("key", key)
 for i in key:
     i = str(i)
     keyfile.write(i)
-
-# def alphacalculator(list,number):
-
 
 savekey = input(" Do you want to save the key? Y/N ")
 while savekey.upper() not in ("Y","N"):
@@ -118,10 +81,7 @@ else:
     print("**************************")
     print("KEY WAS SUCCESSFULLY SAVED")
     print("**************************")
-
-
 decryptmessage = input("Do you want to decrypt your message? Y/N ")
-
 while decryptmessage.upper() not in ("Y","N"):
     decryptmessage = input("Invalid input. Do you want to save the key? Y/N ")
 if decryptmessage.upper() == "N":
@@ -136,43 +96,22 @@ else:
         print("\n\n Decrycpting\n\n\n\n")
         print("Please wait...\n\n")
 
-
-
-# DECRIPTION
-
 t2deindexed = [] 
 for c in encriptedtext2:
     t2deindexed.append(allist.index(c))
-# print("t2deindexed", t2deindexed)
-
 
 dencryptedindexes = []
 withkey = zip(t2deindexed,key)
 for a,b in  withkey:
     dencryptedindexes.append(a+b-len(allist))
-# print("indexes for dcypher", dencryptedindexes)
 
 dencriptedtext = []
 for i in dencryptedindexes:
     dencriptedtext.append(allist[i])
-
-for b,element in enumerate(dencriptedtext):
-    if element == ' ':
-        # print("found")
-        dencriptedtext[b] = "kodiniszodis"
-
-# print("dencryptedtext", dencriptedtext)
-dencriptedtext2 = ' '.join(dencriptedtext)
-
-dencriptedtext2 = dencriptedtext2.replace(" ","").replace("kodiniszodis"," ")
+dencriptedtext2 = "".join(dencriptedtext)
 print("dencriptedtext2",dencriptedtext2)
-
-
-
 print("Congrats. Your message was decrypted. Find the results in program files")
 outputfile.write(dencriptedtext2)
-
 if whattodo.upper() == "FILE":
     openedfile.close()
-
 outputfile.close()
